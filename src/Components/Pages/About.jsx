ede19b931c20c9b980d0607cfcab3f0824d20e2f
Kayla Bristol <bristolk4@gmail.com>
2025-01-12 16:12:34 -0500

    added some css

---
+++ src/Components/Pages/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+
+const About = () => {
+  return (
+    <>
+   <div>About</div>
+    <p>
+        Customize your bakery's info.
+        Find info about your neighboring bakeries.
+        Update your bakery's specialty.
+    </p>
+    </>
+  )
+}
+
+export default About
--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,32 +1,29 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 // import NavBar from "./Components/UI/navbar/NavBar";
+import './Home.css'
+import PhotoGallery from "../UI/modules/PhotoGallery";
 
 const Home = () => {
-  return (
+  
+
+  const navigate = useNavigate()
+    return (
     <>
     {/* <NavBar/> */}
     <div className="home-div">
 
-      <header className="home-header">
-        <h1>DoughtaBase</h1>
-        <p>An address book for bakers and their baking neighbors.</p>
+      <header className="home-header text-center py-5">
+        <h1 className="display-4 fw-bold">DoughtaBase</h1>
+        <p >An address book for bakers and their baking neighbors.</p>
       </header>
 
-        {/* <div className="navbar">
-          <Link to="/specialties" className="nav-link">Specialties</Link>
-          <Link to="/map" className="nav-link">Bakeries Near You</Link>
-        </div> */}
+        <PhotoGallery/>
 
-      <div className="home-about">
-        <h2>About DoughtaBase</h2>
-        <p>
-        Customize your bakery's info.
-        Find info about your neighboring bakeries.
-        Update your bakery's specialty.
-        </p>
-      </div>
-
+        <button className="about-button" onClick={() => {
+            navigate("/about")
+        }}>About</button>
+        
     </div>
     </>    
   );
