c507e0f5bd7803f62a69d42f454a46b8d8f1bcc3
jonaspaulosulit <jonas_sulit2001@yahoo.com>
2025-01-13 21:58:40 -0800

    initial show page design

--- src/Components/UI/modules/BakedGood.jsx
+++ src/Components/UI/modules/BakedGood.jsx
@@ -2,19 +2,16 @@ import React from 'react'
 
 const BakedGood = ({ item }) => {
   return (
-        <div className="card" style={{ width: '18rem' }}>
+        <div className="card" style={{ width: '20rem' }}>
             <img src={item.image_url} className="card-img-top" alt={item.image_url} style={{ width: '8rem' }}/>
             <div className="card-body">
                 <h5
                   className="card-title . text-truncate"
-                  data-bs-toggle="tooltip"
-                  data-bs-placement="top"
-                  data-bs-title="This top tooltip is themed via CSS variables."
                 >
-                    {item.name} $ {item.price}
+                    {item.name}
                 </h5>
 
-                <a href="#" className="btn" style={{ backgroundColor: '#745537', color: 'white' }}>Add to Cart</a>
+                <a href="#" className="btn" style={{ backgroundColor: '#745537', color: 'white' }}>Add to Cart $ {item.price}</a>
                 
             <i className="star" />
             </div>
