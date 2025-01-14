008f3a287efc2615d0f59f29c1fc0fc5508e7e41
Kayla Bristol <bristolk4@gmail.com>
2025-01-14 16:05:08 -0500

    finished login css

--- src/Components/Pages/About.jsx
+++ src/Components/Pages/About.jsx
@@ -3,29 +3,29 @@ import { useNavigate } from 'react-router-dom';
 import './About.css';
 
 const About = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <div className="about-div">
-            <header className="about-header">
-                <h1>About DoughtaBase</h1>
-                <p>An address book for bakers and their baking neighbors.</p>
-            </header>
-            <div className="about-content">
-                <p>Discover what DoughtaBase offers:</p>
-                <ul>
-                    <li>Customize your bakery's info.</li>
-                    <li>Find information about your neighboring bakeries.</li>
-                    <li>Update your bakery's specialties with ease.</li>
-                </ul>
-            </div>
-            <button
-                className="about-back-button"
-                onClick={() => navigate('/')}
-            >
-                Back to Home
-            </button>
+  return (
+    <div className="about-div">
+      <header className="about-header">
+        <h1>About DoughtaBase</h1>
+        <p>An address book for bakers and their baking neighbors.</p>
+        </header>
+      <div className="about-content">
+        <p>Discover what DoughtaBase offers:</p>
+           <ul>
+            <li>Customize your bakery's info.</li>
+            <li>Find information about your neighboring bakeries.</li>
+            <li>Update your bakery's specialties with ease.</li>
+            </ul>
         </div>
+      <button
+        className="about-back-button"
+        onClick={() => navigate('/')}
+        >
+        Back to Home
+        </button>
+    </div>
     );
 };
 
