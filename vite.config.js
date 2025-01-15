9aaa63bd29c08cbcf58900cad77afdee2751bbe7
jonaspaulosulit <jonas_sulit2001@yahoo.com>
2025-01-14 23:47:24 -0800

    https false for now

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,6 @@ export default defineConfig({
   server: {
     host: '0.0.0.0', // Listen on all network interfaces
     port: 5173,      // Default port, can be customized
+    https: false,
   },
 })
