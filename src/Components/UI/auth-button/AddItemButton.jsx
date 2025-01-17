427b3b6f8cd2e5166e45b5db9366be02e9116041
jonaspaulosulit <jonas_sulit2001@yahoo.com>
2025-01-17 01:59:14 -0800

    added additional func

---
+++ src/Components/UI/auth-button/AddItemButton.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react'
+import { useAuth } from '../../../context/AuthContext'
+
+const AddItemButton = ({ bakeryOwner }) => {
+
+  const { user, loading } = useAuth()
+
+  useEffect(() => {
+    console.log(user)
+  })
+
+  if(loading) return <>loading...</>
+  return (
+    <span
+    className='add_span'
+    data-bs-toggle="modal"
+    data-bs-target="#list_menu_item"
+    style={{ display: bakeryOwner === user?.user_id  ? 'block' : 'none' }}
+    >
+        <i class="fa-solid fa-plus"></i>
+    </span>
+  )
+}
+
+export default AddItemButton
